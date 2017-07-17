d50cf98a10b5cbd690e59fe8617265b37066c4a0
Craig Johnston <enochroot@straylikstation.lan>
2017-07-16 22:41:31 -0700

    Updated readme

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+git subsplit update
 git subsplit publish "
     src/DeasilWorks/CEF:git@github.com:deasilworks/cef.git
 " --heads=master
--- src/DeasilWorks/CEF/README.md
+++ src/DeasilWorks/CEF/README.md
@@ -3,3 +3,5 @@ CEF - 4klift Component
 
 The CEF component to 4klift.
 
+**This is alpha code and not ready for production.**
+
