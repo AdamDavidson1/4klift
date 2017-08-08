cc2e6437f8d9e1423bdbed73bcd0e27139a1105f
Craig Johnston <cjimti@gmail.com>
2017-08-08 09:19:11 -0700

    publish case fix

--- util/publish.sh
+++ util/publish.sh
@@ -7,10 +7,10 @@ git subsplit update
 # see https://github.com/dflydev/git-subsplit
 
 git subsplit publish "
-     src/deasilworks/API:git@github.com:deasilworks/api.git
-     src/deasilworks/CEF:git@github.com:deasilworks/cef.git
-     src/deasilworks/CFG:git@github.com:deasilworks/cfg.git
-     src/deasilworks/CMS:git@github.com:deasilworks/cms.git
+     src/deasilworks/api:git@github.com:deasilworks/api.git
+     src/deasilworks/cef:git@github.com:deasilworks/cef.git
+     src/deasilworks/cfg:git@github.com:deasilworks/cfg.git
+     src/deasilworks/cms:git@github.com:deasilworks/cms.git
      core:git@github.com:deasilworks/4klift-core.git
      skeleton-se:git@github.com:deasilworks/4klift-se.git
  " --heads=master
